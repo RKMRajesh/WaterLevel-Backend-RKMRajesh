4daba70d7c4a86c81f812fdbc9937cc8aeb411f5
RKMRajesh <50008817+RKMRajesh@users.noreply.github.com>
2022-06-02 01:59:07 +0000

    Made /getCDECData work with request query parameters, also turned the callback function for getCDEC data to an async function so that await works.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const express = require("express");
 // create object to interface with express
 const app = express();
-
+const fetch = require("cross-fetch");
 // Code in this section sets up an express pipeline
 
 // print info about incoming HTTP request 
@@ -23,6 +23,18 @@ app.get("/query/getResponse", (req, res, next) => {
   res.json({msg: "Here is your response!"});
 });
 
+app.get("/query/getCDECData", async (req, res, next) => {
+  console.log("Received /getCDECData request from React server");
+  
+  const month = req.query.month;
+  const year = req.query.year;
+  // const month = 4;
+  // const year = 2022;
+  let waterData = await getWaterData(month, year);
+  res.json(waterData);
+  
+  // res.json({msg: "Here is your response!"});
+});
 
 // respond to all AJAX querires with this message
 app.use(function(req, res, next) {
@@ -36,3 +48,11 @@ app.use(function(req, res, next) {
 const listener = app.listen(3000, function () {
   console.log("The static server is listening on port " + listener.address().port);
 });
+
+async function getWaterData(month, year) {
+  const apiUrl =  `https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet?Stations=SHA,ORO,CLE,NML,SNL,DNP,BER&SensorNums=15&dur_code=M&Start=${year}-${month}&End=${year}-${month}`;
+  // send it off
+  let fetchResponse = await fetch(apiUrl);
+  let data = await fetchResponse.json()
+  return data;
+}
